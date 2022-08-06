cb13eebb5fc95da7a19040e09b5e7b2c8201d098
unknown <sandrasobel95@gmail.com>
2022-08-06 10:17:23 +0100

    13.1 settings gradle poprawiony

---
+++ kodillla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -0,0 +1,6 @@
+package com.kodilla.good.patterns.challenges;
+
+public class ProductOrderService {
+    //wykonywanie zamowien roznego rodzaju produkty
+    //interfesjy: information service/czyjestnastanie/datazamowienia
+}
